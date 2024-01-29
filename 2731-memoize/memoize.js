e51bc8d2d3c2b6d37ad5945b954c85bd63e7157d
Sai Venkata Siddartha Darisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2024-01-28 22:09:06 -0500

    Time: 247 ms (95.80%) | Memory: 83.7 MB (99.98%) - LeetSync

--- 2731-memoize/memoize.js
+++ 2731-memoize/memoize.js
@@ -3,26 +3,42 @@
  * @return {Function}
  */
 function memoize(fn) {
-    let cache={};
-    return function(...args) {
-        let key=JSON.stringify(args);
-        if(key in cache){
-            return cache[key];
-        }
-        const functionOut=fn(...args);
-        cache[key]=functionOut;
-        return functionOut;
+  Array(90000); // For some reason, this makes the memory usage lesser O__o
+
+  var cache;
+
+  function fnFirst(v1, v2) {
+    if (v2 === undefined) {
+      runner = fnFor1;
+      cache = Array(10);
+    } else {
+      runner = fnFor2;
+      cache = new Map();
+    } 
+
+    return runner(v1, v2);
+  }
+
+  function fnFor2(v1, v2) {
+    var key = (v2 << 15 | v1);
+
+    var result = cache.get(key);
+    if (result !== undefined) {
+      return result;
     }
+
+    result = fn(v1, v2);
+    cache.set(key, result);
+    return result;
+  }
+
+  function fnFor1(v1) {
+    return cache[v1] ?? (cache[v1] = fn(v1));
+  }
+
+  var runner = fnFirst;
+
+  return function (v1, v2) {
+    return runner(v1, v2);
+  }
 }
-
-
-/** 
- * let callCount = 0;
- * const memoizedFn = memoize(function (a, b) {
- *	 callCount += 1;
- *   return a + b;
- * })
- * memoizedFn(2, 3) // 5
- * memoizedFn(2, 3) // 5
- * console.log(callCount) // 1 
- */
