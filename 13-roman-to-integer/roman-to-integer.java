bfaa9f35af96405787dd112871f9518bb4872f96
Sai Venkata Siddartha Darisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2024-01-30 01:51:59 -0500

    Time: 2 ms (100.00%) | Memory: 44.6 MB (54.95%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,65 +1,26 @@
 class Solution {
-    public int romanToInt(String s) {
-        if (s.length() == 0) {
-            return 0;
+   public int romanToInt(String s) {
+    
+    int answer = 0, number = 0, prev = 0;
+
+    for (int j = s.length() - 1; j >= 0; j--) {
+        switch (s.charAt(j)) {
+            case 'M' -> number = 1000;
+            case 'D' -> number = 500;
+            case 'C' -> number = 100;
+            case 'L' -> number = 50;
+            case 'X' -> number = 10;
+            case 'V' -> number = 5;
+            case 'I' -> number = 1;
         }
-
-        int number = 0;
-        int length = s.length();
-
-        for (int i = 0; i < length; i++) {
-            char c = s.charAt(i);
-            number += switch (c) {
-                case 'I' -> {
-                    if (i + 1 < length && (s.charAt(i + 1) == 'V' || s.charAt(i + 1) == 'X')) {
-                        if (s.charAt(i + 1) == 'V') {
-                            i++;
-                            yield 4;
-                        } else {
-                            i++;
-                            yield 9;
-                        }
-                    } else {
-                        yield 1;
-                    }
-                }
-                case 'V' -> 5;
-                case 'X' -> {
-                    if (i + 1 < length && (s.charAt(i + 1) == 'L' || s.charAt(i + 1) == 'C')) {
-                        if (s.charAt(i + 1) == 'L') {
-                            i++;
-                            yield 40;
-                        } else {
-                            i++;
-                            yield 90;
-                        }
-                    } else {
-                        yield 10;
-                    }
-                }
-                case 'L' -> 50;
-                case 'C' -> {
-                    if (i + 1 < length && (s.charAt(i + 1) == 'D' || s.charAt(i + 1) == 'M')) {
-                        if (s.charAt(i + 1) == 'D') {
-                            i++;
-                            yield 400;
-                        } else {
-                            i++;
-                            yield 900;
-                        }
-                    } else {
-                        yield 100;
-                    }
-                }
-                case 'D' -> 500;
-                case 'M' -> 1000;
-                default -> 0;
-            };
-
-            if (i >= length - 1) {
-                break;
-            }
+        if (number < prev) {
+            answer -= number;
         }
-        return number;
+        else {
+            answer += number;
+        }
+        prev = number;
     }
+    return answer;
+}
 }
