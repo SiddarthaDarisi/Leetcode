0875773cc1f5e079f2a15529398e987495f6e6e0
Sai Venkata Siddartha Darisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2024-01-31 10:38:11 -0500

    Time: 2 ms (91.91%) | Memory: 47.3 MB (16.12%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -3,18 +3,17 @@ class Solution {
       int a,b;
        int i=0,j=numbers.length-1;
        while(i<j){
-           a=numbers[i];
-           b=numbers[j];
-           if(a+b==target){
+          int sum=numbers[i]+numbers[j];
+           if(sum==target){
                break;
            }
-           else if(a+b>target){
+           else if(sum>target){
                j--;
            }
-           else if(a+b<target){
+           else 
                i++;
            }
-       } 
+       
         
         
        return  new int[]{i+1, j+1};
