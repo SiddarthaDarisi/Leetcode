3ef03fbb33d22da2e551935008ac00fe05e1994f
Siddartha Darisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2024-06-12 15:13:48 -0700

    Time: 1 ms (100.00%) | Memory: 57.1 MB (54.12%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,20 +1,31 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-        boolean[] check = new boolean[n + 1];
+        int n = nums.length, size = 0;
+
+        while(n > 0){
+            n = n>>1;
+            size++;
+        }
+        n = nums.length;
+        int pivot = 0;
         
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0 && nums[i] <= n) {
-                check[nums[i]] = true;
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] <= 0 || nums[i] > n){
+                int temp = nums[i];
+                nums[i] = nums[pivot];
+                nums[pivot] = temp;
+                pivot++;
             }
         }
-        
-        for (int i = 1; i <= n; i++) {
-            if (!check[i]) {
-                return i;
-            }
+        for(int i= 0; i < pivot; i++)
+            nums[i] = 0;
+        for(int i= pivot; i < n; i++){
+            nums[(nums[i] - 1)&((1<<size) - 1)] |= (1<<size);
         }
-        
-        return n + 1;
+        for(int i= 0; i < n; i++)
+            if((nums[i] & (1<<size)) == 0)
+                return i+1;
+        return n+1;
     }
 }
