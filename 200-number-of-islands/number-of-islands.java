1ea4acee31d6fa9f7c5f65d21067be5e4b7d1aca
Siddartha Darisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2024-06-11 20:46:19 -0700

    Time: 3 ms (84.96%) | Memory: 49.2 MB (64.18%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,33 +1,31 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int count=0;
-        HashMap<Integer,Integer> visit=new HashMap<>();
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]=='1'){
-                boolean check=dfs(grid,i,j,visit);
-                if(check==true)count++;
+                dfs(grid,i,j);
+                count++;
              }
             }
         }
         return count;
     }
-    public boolean dfs(char[][] grid,int i,int j,HashMap<Integer,Integer> visit){
+    public void dfs(char[][] grid,int i,int j){
        if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0') {
-    return false;
+    return ;
 }
         if(grid[i][j]=='1'){
-            visit.put(i,j);
+           
             grid[i][j]='0';
-            dfs(grid,i+1,j,visit);
-            dfs(grid,i-1,j,visit);
-            dfs(grid,i,j+1,visit);
-            dfs(grid,i,j-1,visit);
+            dfs(grid,i+1,j);
+            dfs(grid,i-1,j);
+            dfs(grid,i,j+1);
+            dfs(grid,i,j-1);
             
-            if(visit.isEmpty())
-                return true;
+            
         }
         
-        return true;
+    
     }
 }
