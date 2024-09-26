ec8c6f712b0e5e0908f517eca8c697083b4a6144
Siddartha Darisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2024-09-26 08:46:31 -0700

    Time: 6 ms (43.38%) | Memory: 43.1 MB (75.72%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,18 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-    if (s.length() != t.length()) {
-        return false;
-    }
-    int[] table = new int[26];
-    for (int i = 0; i < s.length(); i++) {
-        table[s.charAt(i) - 'a']++;
-    }
-    for (int i = 0; i < t.length(); i++) {
-        table[t.charAt(i) - 'a']--;
-        if (table[t.charAt(i) - 'a'] < 0) {
+        if (s.length()!=t.length())
+        {
             return false;
         }
+        int[] a= new int[26];
+        
+        for(int i=0;i<s.length();i++){
+            a[(((int)s.charAt(i)-'a') )]++;
+            a[(((int)t.charAt(i)-'a'))]--;
+        }
+        for(int i=0;i<a.length;i++){
+            if(a[i]!=0){
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
-}
 }
