8f5d1182e8eb1d30de8faeb2c262de567a55ffef
Siddartha Darisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2025-03-09 12:39:34 -0700

    Time: 1 ms (94.92%) | Memory: 45.5 MB (45.76%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+     int[] ans= new int[nums.length*2];
+     for(int i=0,j=nums.length;i<nums.length;i++,j++){
+        ans[i]=nums[i];
+        ans[j]=nums[i];
+     }   
+     return ans;
+    }
+}
