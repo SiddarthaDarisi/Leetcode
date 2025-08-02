7f31d01b07731d8874e8317531f103c78c239ead
Siddartha Darisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2025-08-02 16:02:21 -0700

    Time: 0 ms (100.00%) | Memory: 40.2 MB (81.87%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,17 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n <= 3) return n;
-
-        int prev1 = 3;
-        int prev2 = 2;
-        int cur = 0;
-
-        for (int i = 3; i < n; i++) {
-            cur = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = cur;
+       if (n == 1) {
+            return 1;
         }
+        int[] arr = new int[n + 1];
+        arr[1] = 1;// one jump
+        arr[2] = 2;// two or one 
 
-        return cur;        
+        for(int i = 3; i <= n; i++) { 
+            arr[i] = arr[i-1] + arr[i-2];
+            // the number of ways to reach the n th level is the sum of two previos levels
+        }
+        return arr[n];
     }
 }
